cc5405faa0f57bf386c1c98ea7fc69092755c896
LOBSTR <85858522+spaulll@users.noreply.github.com>
2024-07-30 18:42:01 +0000

    feat: contract deployed

--- main.sh
+++ main.sh
@@ -130,7 +130,7 @@ async function main() {
   const contractFactory = await hre.ethers.getContractFactory("Swisstronik");
   const contract = contractFactory.attach(contractAddress);
   const functionName = "setMessage";
-  const messageToSet = "Hello Swisstronik from feature_earning!!";
+  const messageToSet = "Hello Swisstronik from lobstr!!";
   const setMessageTx = await sendShieldedTransaction(signer, contractAddress, contract.interface.encodeFunctionData(functionName, [messageToSet]), 0);
   await setMessageTx.wait();
   console.log("Transaction Receipt: ", setMessageTx);
